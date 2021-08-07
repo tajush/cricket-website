511742b4b52ceb67fb9543e46ec50e0a184b5db6
tajush <tajusabd@gmail.com>
2021-08-07 23:17:52 +0600

    updated

---
+++ java-script/index.js
@@ -0,0 +1,3 @@
+document.write("<h1>ldddddddddove<h1><br>");
+document.write("<h1>love bd<h1>");
+console.log(document)
---
+++ java-script/index2.js
@@ -0,0 +1,15 @@
+/*document.write("abdur rahman")
+document.write("abdur rahman")
+document.write("abdur rahman")
+document.write("abdur rahman")
+document.write("abdur rahman")
+document.write("abdur rahman")*/
+document.write("abdur rahman")
+var namee = "20.5";
+namee=toString(namee)
+namee=parseInt(namee)
+namee=parseFloat(namee)
+console.log(typeof(namee));
+var number=2.567;
+console.log(number.toPrecision(2))
+console.log(Number(false))
